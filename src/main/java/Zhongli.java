04807bd209c9b4f05ba2a163a8581c083731bbfd
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-01-17 15:09:21 +0800

    Added Greeting Message

---
+++ src/main/java/Zhongli.java
@@ -0,0 +1,14 @@
+public class Zhongli {
+
+    public static void printHorizontalLine() {
+        String horizontalLine = "_____________________________________________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Zhongli");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+    }
+}

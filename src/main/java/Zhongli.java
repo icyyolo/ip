843b744ebbdf853582a2cfd8fc10770d08de8a3a
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-01-17 15:22:16 +0800

    Implemented Echo Functionality

--- src/main/java/Zhongli.java
+++ src/main/java/Zhongli.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Zhongli {
 
     public static void printHorizontalLine() {
@@ -6,9 +8,18 @@ public class Zhongli {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         printHorizontalLine();
         System.out.println("Hello! I'm Zhongli");
         System.out.println("What can I do for you?");
+        printHorizontalLine();
+        String userInput = input.nextLine();
+        while (!userInput.equals("bye")) {
+            printHorizontalLine();
+            System.out.println(userInput);
+            printHorizontalLine();
+            userInput = input.nextLine();
+        }
         printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
         printHorizontalLine();

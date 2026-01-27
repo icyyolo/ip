5b9dbcae72fa5ba668d05bf108720855e51e3d5c
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-01-27 09:43:02 +0800

    Add more test cases to ToDoTest.java

--- src/test/java/Zhongli/Task/ToDoTest.java
+++ src/test/java/Zhongli/Task/ToDoTest.java
@@ -13,14 +13,14 @@ public class ToDoTest {
     }
 
     @Test
-    public void markDone_success() {
+    public void markDone_toString_success() {
         ToDo todo = new ToDo("test");
         todo.markDone();
         assertEquals("[T][X] test", todo.toString());
     }
 
     @Test
-    public void markUndone_success() {
+    public void markUndone_toString_success() {
         ToDo todo = new ToDo("test");
         todo.markUndone();
         assertEquals("[T][ ] test", todo.toString());
@@ -30,6 +30,26 @@ public class ToDoTest {
     public void getDescription_success() {
         ToDo todo = new ToDo("test");
         assertEquals("test", todo.getDescription());
+    }
+
+    @Test
+    public void convertToText_success() {
+        ToDo todo = new ToDo("test");
+        assertEquals("todo test/unmark\n", todo.convertToText());
+    }
+
+    @Test
+    public void convertToText_markDone_success() {
+        ToDo todo = new ToDo("test");
+        todo.markDone();
+        assertEquals("todo test/mark\n", todo.convertToText());
+    }
+
+    @Test
+    public void convertToText_markUndone_success() {
+        ToDo todo = new ToDo("test");
+        todo.markUndone();
+        assertEquals("todo test/unmark\n", todo.convertToText());
     }
 }
 
